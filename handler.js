82db8908806d330e3159e3ee65faa89edb1cf2bb
Julian Salas <juliansalas080@gmail.com>
2019-03-06 00:36:18 -0500

    added a test endpoint aws

--- handler.js
+++ handler.js
@@ -1,6 +1,19 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
+module.exports.test = async(event, context, callback) => {
+  console.log(event);
+  const response = {
+    statusCode: 200,
+    headers: {
+      "x-custom-header" : "My Header Value"
+    },
+    body:JSON.stringify({'message': 'Hello world'})
+  };
+  callback(null, response)
+}
+
+
+module.exports.send = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
